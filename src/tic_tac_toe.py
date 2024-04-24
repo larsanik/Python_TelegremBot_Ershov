9fab72c6a065ed245a5da1381e1470028002f123
larsanik <larsanik@gmail.com>
2024-04-24 16:38:17 +0300

    task2 attempt 2

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,23 +1,27 @@
 from colorama import Fore, Back, Style
 
 
-def draw_board(board, sze):
+def draw_board(clone_board, sze):
+    # делаем клон доски
+    # clone_board = board[:] todo
     # запустить цикл, который проходит по всем  строкам доски
     for i in range(sze):
         # разукращиваем символы в строке
         for ii in range(sze):
-            if board[i][ii] == ' ':
-                 board[i][ii] = Back.MAGENTA + board[i][ii] + Style.RESET_ALL
-            if board[i][ii] == '0':
-                 board[i][ii] = Fore.BLUE + board[i][ii] + Style.RESET_ALL
-            if board[i][ii] == 'X':
-                 board[i][ii] = Fore.RED + board[i][ii] + Style.RESET_ALL
+            if clone_board[i][ii] == ' ':
+                clone_board[i][ii] = Back.MAGENTA + clone_board[i][ii] + Style.RESET_ALL
+            if clone_board[i][ii] == '0':
+                clone_board[i][ii] = Fore.BLUE + clone_board[i][ii] + Style.RESET_ALL
+            if clone_board[i][ii] == 'X':
+                clone_board[i][ii] = Fore.RED + clone_board[i][ii] + Style.RESET_ALL
 
         # line = Back.MAGENTA + " | ".join(board[i]) + Style.RESET_ALL + '\n'
-        # print(line)
+        print(id(clone_board))
+        #print(id(board))
+
 
         # поставить разделители значений в строке
-        print(" | ".join(board[i]))
+        print(" | ".join(clone_board[i]))
         # print((Back.MAGENTA + " " + Style.RESET_ALL + "| ").join(board[i]))
 
         # поставить разделители строк
@@ -37,6 +41,8 @@ def ask_move(player, board):
         # если свободно, вернуть координаты
         return (x, y)
     else:
+        print(board[y][x])  # todo убрать контрольный вывод
+        print(id(board))  # todo убрать контрольный вывод
         print(f"Клетка {x} {y} занята. Попробуйте еще раз.")
         return ask_move(player, board)
 
@@ -93,7 +99,8 @@ def tic_tac_toe(sze):
         # бесконечный цикл раунда
         while True:
             # рисуем игровое поле
-            draw_board(board, sze)
+            clone_board = board[:]
+            draw_board(clone_board, sze)
             # запросить ход
             ask_and_make_move(player, board)
             # проверка выигрыша
