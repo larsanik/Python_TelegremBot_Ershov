b5ba98d225f0abdbe37827a9a2d822a7cee32d22
Larsanik <larsanik@gmail.com>
2024-06-19 01:07:53 +0300

    task4 finish

--- src/notepad2.py
+++ src/notepad2.py
@@ -8,9 +8,11 @@
 
 import os
 import time
+from glob import glob
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def build_note(note_text, note_name):
     """получает название и текст заметки, а затем создает текстовый файл с этим названием и текстом"""
@@ -70,6 +72,20 @@ def delete_note():
 
 def display_notes():
     """выводит все заметки пользователя"""
+    # формирование списка файлов с замктками
+    notes = [note for note in os.listdir() if note.endswith(".txt")]
+    # создание словаря с именем файла и длинной заметки
+    dic_notes = {}
+    for i in notes:
+        with open(i, "r", encoding="utf-8") as file:
+            dic_notes[i] = len(file.read())
+    # сортировка заметок по возрастанию длинны
+    sorted_notes = sorted(dic_notes.items(), key=lambda item: item[1], reverse=False)
+    # вывод заметок в порядке возрастания длинны
+    for i in sorted_notes:
+        with open(i[0], "r", encoding="utf-8") as file:
+            print(f'Заметка "{i[0]}".')
+            print(file.read(), '\n')
 
 
 def main():
@@ -103,4 +119,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main() todo только для задания 4
+    display_notes()  # todo только для задания 4
+
