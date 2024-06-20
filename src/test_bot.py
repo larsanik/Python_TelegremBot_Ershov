480fb4ac97cbe9933f5e985d507b047fd680790d
larsanik <larsanik@gmail.com>
2024-06-20 16:49:04 +0300

    task7 dev_process_2

--- src/test_bot.py
+++ src/test_bot.py
@@ -18,28 +18,63 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+# шаги ввода данных
 NAME, TEXT = range(2)
 
+# глобалки для передачи значений
+note_name, note_text = '', ''
 
-def get_name_note(update: Update, context: CallbackContext) -> int:
+# todo Добавить во все функции обработчики ошибок
+# todo Добавить надписи в поле ввода, как в примере с гендерами)
+
+
+
+def build_note(note_text, note_name):
+    """получает название и текст заметки, а затем создает текстовый файл с этим названием и текстом"""
+    try:
+        if not note_text.strip():
+            logger.error("Текст заметки не может быть пустым.")
+            return
+        with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
+            file.write(note_text)
+        logger.info(f"Заметка {note_name} создана.")
+    except Exception as err:
+        logger.error(f'Произошла ошибка: {err}')
+
+
+# запуск ввода данных для создания заметок
+def create_note_handler(update: Update, context: CallbackContext) -> int:
     """Запрос имени заметки."""
     update.message.reply_text('Введите имя заметки: ')
     return NAME
 
 
-def get_text_note(update: Update, context: CallbackContext) -> int:
+def get_name_note(update: Update, context: CallbackContext) -> int:
     """Запрос текста заметки."""
+    user = update.message.from_user
+    logger.info(f"Пользователь:  {user.first_name}. Имя заметки: {update.message.text}.")
+    global note_name
+    note_name = update.message.text
     update.message.reply_text('Введите текст заметки: ')
     return TEXT
+
+
+def get_text_note(update: Update, context: CallbackContext) -> int:
+    """Выход из опроса."""
+    user = update.message.from_user
+    logger.info(f"Пользователь:  {user.first_name}. Текст заметки: {update.message.text}")
+    global note_text
+    note_text = update.message.text
+    build_note(note_text, note_name) # создаем заметку
+    update.message.reply_text(f'Заметка {note_name} создана.')
+    return ConversationHandler.END
 
 
 def cancel(update: Update, context: CallbackContext) -> int:
     """Выход из диалога по команде /cancel."""
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text(
-        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
-    )
+    logger.info(f"Пользователь {user.first_name} вышел из диалога .")
+    update.message.reply_text('Запрос данных прерван пользователем')
 
     return ConversationHandler.END
 
@@ -54,7 +89,7 @@ def main() -> None:
 
     # Add conversation handler with the states NAME, ТEXT, LOCATION and BIO
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('create', get_name_note)],
+        entry_points=[CommandHandler('create', create_note_handler)],
         states={
             NAME: [MessageHandler(Filters.text, get_name_note)],
             TEXT: [MessageHandler(Filters.text, get_text_note)],
@@ -67,7 +102,7 @@ def main() -> None:
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-F2 or the process receives SIGINT,
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
