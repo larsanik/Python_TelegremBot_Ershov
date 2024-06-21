f4bdf40f4076b6cc9d214045d465c57c4b7aff8e
larsanik <larsanik@gmail.com>
2024-06-21 15:41:05 +0300

    task7 dev_process_4

--- src/test_bot.py
+++ src/test_bot.py
@@ -24,6 +24,7 @@ NAME, TEXT = range(2)
 # глобалки для передачи значений
 note_name, note_text = '', ''
 
+
 # todo Добавить во все функции обработчики ошибок
 # todo Добавить надписи в поле ввода, как в примере с гендерами)
 # todo Проверить работу преждевременного выхода по /cancel
@@ -35,7 +36,7 @@ def build_note(note_text, note_name):
     try:
         if not note_text.strip():
             logger.error("Текст заметки не может быть пустым.")
-            return
+            return "Текст заметки не может быть пустым."
         with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
             file.write(note_text)
         logger.info(f"Заметка {note_name} создана.")
@@ -46,70 +47,95 @@ def build_note(note_text, note_name):
 # запуск ввода данных для создания заметок
 def create_note_handler(update: Update, context: CallbackContext) -> int:
     """Запрос имени заметки."""
-    update.message.reply_text('Введите имя заметки: ')
-    return NAME
+    try:
+        update.message.reply_text('Введите имя заметки: ')#,
+        #                           reply_markup=ReplyKeyboardMarkup([''],
+        #                                                            one_time_keyboard=True,
+        #                                                            input_field_placeholder='<Имя заметки>')
+        #                           )
+        update.message.reply_text('Клавиатура скрыта', reply_markup=ReplyKeyboardRemove())
+        return NAME
+    except Exception as err:
+        logger.error(f'Произошла ошибка: {err}')
 
 
 def get_name_note(update: Update, context: CallbackContext) -> int:
     """Запрос текста заметки."""
-    user = update.message.from_user
-    logger.info(f"Пользователь:  {user.first_name}. Имя заметки: {update.message.text}.")
-    global note_name
-    note_name = update.message.text
-    update.message.reply_text('Введите текст заметки: ')
-    return TEXT
+    try:
+        user = update.message.from_user
+        logger.info(f"Пользователь:  {user.first_name}. Имя заметки: {update.message.text}.")
+        global note_name
+        note_name = update.message.text
+        update.message.reply_text('Введите текст заметки: ')#,
+        #                           reply_markup=ReplyKeyboardMarkup([''],
+        #                                                            one_time_keyboard=True,
+        #                                                            input_field_placeholder='<Текст заметки>')
+        #                           )
+        update.message.reply_text('Клавиатура скрыта', reply_markup=ReplyKeyboardRemove())
+        return TEXT
+    except Exception as err:
+        logger.error(f'Произошла ошибка: {err}')
 
 
 def get_text_note(update: Update, context: CallbackContext) -> int:
     """Выход из опроса."""
-    user = update.message.from_user
-    logger.info(f"Пользователь:  {user.first_name}. Текст заметки: {update.message.text}")
-    global note_text
-    note_text = update.message.text
-    build_note(note_text, note_name) # создаем заметку
-    update.message.reply_text(f'Заметка {note_name} создана.')
-    return ConversationHandler.END
+    try:
+        user = update.message.from_user
+        logger.info(f"Пользователь:  {user.first_name}. Текст заметки: {update.message.text}")
+        global note_text
+        note_text = update.message.text
+        build_note(note_text, note_name)  # создаем заметку
+        if build_note(note_text, note_name):  # сообщение заметка не может быть пустая
+            update.message.reply_text(build_note(note_text, note_name))
+        else:
+            update.message.reply_text(f'Заметка {note_name} создана.')
+            return ConversationHandler.END
+    except Exception as err:
+        logger.error(f'Произошла ошибка: {err}')
 
 
 def cancel(update: Update, context: CallbackContext) -> int:
     """Выход из диалога по команде /cancel."""
-    user = update.message.from_user
-    logger.info(f"Пользователь {user.first_name} вышел из диалога .")
-    update.message.reply_text('Запрос данных прерван пользователем')
-
-    return ConversationHandler.END
+    try:
+        user = update.message.from_user
+        logger.info(f"Пользователь {user.first_name} вышел из диалога .")
+        update.message.reply_text('Запрос данных прерван пользователем')
+        return ConversationHandler.END
+    except Exception as err:
+        logger.error(f'Произошла ошибка: {err}')
 
 
 def main() -> None:
     """Run the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater(token=secrets.API_TOKEN)
+    try:
+        # Create the Updater and pass it your bot's token.
+        updater = Updater(token=secrets.API_TOKEN)
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+        # Get the dispatcher to register handlers
+        dispatcher = updater.dispatcher
 
-    # Add conversation handler with the states NAME, ТEXT, LOCATION and BIO
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('create', create_note_handler)],
-        states={
-            NAME: [MessageHandler(Filters.text, get_name_note)],
-            TEXT: [MessageHandler(Filters.text, get_text_note)],
-        },
-        fallbacks=[CommandHandler('cancel', cancel)],
-    )
+        # Add conversation handler with the states NAME, ТEXT, LOCATION and BIO
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('create', create_note_handler)],
+            states={
+                NAME: [MessageHandler(Filters.text, get_name_note)],
+                TEXT: [MessageHandler(Filters.text, get_text_note)],
+            },
+            fallbacks=[CommandHandler('cancel', cancel)],
+        )
 
-    dispatcher.add_handler(conv_handler)
+        dispatcher.add_handler(conv_handler)
 
-    # Start the Bot
-    updater.start_polling()
+        # Start the Bot
+        updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        updater.idle()
+    except Exception as err:
+        logger.error(f'Произошла ошибка: {err}')
 
 
 if __name__ == '__main__':
     main()
-
-
